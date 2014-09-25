aaf4933d5cc67d284e48078880efddc587c5a5c1
getnamo <getnamo@getnamo.com>
2014-09-25 18:01:46 +0100

    C++ input mapping support fix
    -Moved Hydra FKey definitions and object code to the Hydra Delegate.
    -HydraDelegate updated, if you're using a custom class in C++ deriving
    from the delegate update your delegate copy in your project folder.

--- Plugins/HydraPlugin/Source/HydraPlugin/Private/FHydraPlugin.cpp
+++ Plugins/HydraPlugin/Source/HydraPlugin/Private/FHydraPlugin.cpp
@@ -27,47 +27,6 @@ dll_sixenseInit HydraInit;
 dll_sixenseExit HydraExit;
 dll_sixenseGetAllNewestData HydraGetAllNewestData;
 
-//Define each FKey const in a .cpp so we can compile
-const FKey EKeysHydra::HydraLeftJoystickX("HydraLeftJoystickX");
-const FKey EKeysHydra::HydraLeftJoystickY("HydraLeftJoystickY");
-const FKey EKeysHydra::HydraLeftJoystickClick("HydraLeftJoystickClick");
-const FKey EKeysHydra::HydraLeftB1("HydraLeftB1");
-const FKey EKeysHydra::HydraLeftB2("HydraLeftB2");
-const FKey EKeysHydra::HydraLeftB3("HydraLeftB3");
-const FKey EKeysHydra::HydraLeftB4("HydraLeftB4");
-const FKey EKeysHydra::HydraLeftStart("HydraLeftStart");
-const FKey EKeysHydra::HydraLeftTrigger("HydraLeftTrigger");
-const FKey EKeysHydra::HydraLeftTriggerClick("HydraLeftTriggerAxis");
-const FKey EKeysHydra::HydraLeftBumper("HydraLeftBumper");
-
-const FKey EKeysHydra::HydraLeftMotionX("HydraLeftMotionX");
-const FKey EKeysHydra::HydraLeftMotionY("HydraLeftMotionY");
-const FKey EKeysHydra::HydraLeftMotionZ("HydraLeftMotionZ");
-
-const FKey EKeysHydra::HydraLeftRotationPitch("HydraLeftRotationPitch");
-const FKey EKeysHydra::HydraLeftRotationYaw("HydraLeftRotationYaw");
-const FKey EKeysHydra::HydraLeftRotationRoll("HydraLeftRotationRoll");
-
-const FKey EKeysHydra::HydraRightJoystickX("HydraRightJoystickX");
-const FKey EKeysHydra::HydraRightJoystickY("HydraRightJoystickY");
-const FKey EKeysHydra::HydraRightJoystickClick("HydraRightJoystickClick");
-const FKey EKeysHydra::HydraRightB1("HydraRightB1");
-const FKey EKeysHydra::HydraRightB2("HydraRightB2");
-const FKey EKeysHydra::HydraRightB3("HydraRightB3");
-const FKey EKeysHydra::HydraRightB4("HydraRightB4");
-const FKey EKeysHydra::HydraRightStart("HydraRightStart");
-const FKey EKeysHydra::HydraRightTrigger("HydraRightTrigger");
-const FKey EKeysHydra::HydraRightTriggerClick("HydraRightTriggerAxis");
-const FKey EKeysHydra::HydraRightBumper("HydraRightBumper");
-
-const FKey EKeysHydra::HydraRightMotionX("HydraRightMotionX");
-const FKey EKeysHydra::HydraRightMotionY("HydraRightMotionY");
-const FKey EKeysHydra::HydraRightMotionZ("HydraRightMotionZ");
-
-const FKey EKeysHydra::HydraRightRotationPitch("HydraRightRotationPitch");
-const FKey EKeysHydra::HydraRightRotationYaw("HydraRightRotationYaw");
-const FKey EKeysHydra::HydraRightRotationRoll("HydraRightRotationRoll");
-
 //Collector class contains all the data captured from .dll and delegate data will point to this structure (allDataUE and historicalDataUE).
 class DataCollector
 {
